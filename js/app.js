32cf3e3665c70c0a95661bdcd3a1102d3d7c78e1
jlin218 <jlin218@uw.edu>
2014-11-13 01:35:29 +0800

    Revert "add lines"
    This reverts commit cc5233d7cf3f78b110639791a46149b3621e067d.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,7 @@
-"use strict";
+/* 
+    app.js
+    Angular application for the address book challenge
 
-var addressBook = angular.module('AddressBook', []);
-addressBook.controller('AddressController', ['$scope', function($scope) {
-    $scope.employees = pawneeEmployees;
-    $scope.order = 'lastName';
-    $scope.orderedBy = function(colName){
-		$scope.order = colName;
-	};
-	$scope.isOrderedBy = function(colName){
-		return $scope.order == colName;
-	};
-}]);
+    Add code here to create a new Angular application and controller.
+    The array of employee objects is already in a global variable named 'pawneeEmployees'
+*/
